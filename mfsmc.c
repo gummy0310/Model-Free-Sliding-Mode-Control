47e6794b0a973f2db83bca907b5c4617e005400e
gummy0310 <seojin0310@g.skku.edu>
2026-01-26 11:49:47 +0900

    등호 수정

--- mfsmc.c
+++ mfsmc.c
@@ -62,13 +62,13 @@ float Calculate_PID(PID_Param_TypeDef* pid_param, float current_temp, uint8_t ch
     float error_dot = filtered_error_dot[channel];
 
     // 상태에 따른 gain scheduling
-    if (error < 0 && error_dot <0) {
+    if (error < 0 && error_dot > 0) {
         //[case1: 온도가 목표온도 이상이고, 목표온도로 감소중일때]
         lambda = MFSMC_LAMBDA_COOL;
     } else {
         lambda = MFSMC_LAMBDA_HEAT;
     }
-    
+
     alpha = MFSMC_ALPHA
 
     // Time Delay Estimation (F_hat 추정: 현재 상태 유지에 필요한 힘)
